1fb0a341cd6d026bd97578ec70234d6b6b7a7f2d
ascens1ve <silensteam@mail.ru>
2025-04-30 23:15:08 +0300

    Add functionality

--- src/components/arrow-button/ArrowButton.tsx
+++ src/components/arrow-button/ArrowButton.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, forwardRef } from 'react';
 import clsx from 'clsx';
 import arrow from 'src/images/arrow.svg';
 
@@ -7,7 +7,18 @@ import styles from './ArrowButton.module.scss';
 /** Функция для обработки открытия/закрытия формы */
 export type OnClick = () => void;
 
-export const ArrowButton = () => {
+export interface ArrowButtonProps {
+	clickCall: OnClick;
+}
+
+export const ArrowButton = forwardRef<HTMLDivElement | null, ArrowButtonProps>(({clickCall}, ref) => {
+
+	const handleClick = (event: React.MouseEvent<HTMLDivElement>) => {
+		event.currentTarget.classList.toggle(styles.container_open);
+		event.currentTarget.querySelector('img')?.classList.toggle(styles.arrow_open);
+		clickCall();
+	}
+
 	return (
 		/* Не забываем указаывать role и aria-label атрибуты для интерактивных элементов */
 		<div
@@ -15,6 +26,8 @@ export const ArrowButton = () => {
 			aria-label='Открыть/Закрыть форму параметров статьи'
 			tabIndex={0}
 			className={styles.container}
+			onClick={handleClick}
+			ref={ref}
 		>
 			<img
 				src={arrow}
@@ -23,4 +36,4 @@ export const ArrowButton = () => {
 			/>
 		</div>
 	);
-};
+});
